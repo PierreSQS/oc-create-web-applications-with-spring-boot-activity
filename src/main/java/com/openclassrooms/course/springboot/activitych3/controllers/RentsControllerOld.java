97cf1c4569fc02759a83a0d91b7dc7d23946788f
PierreSQS <nomo_messina@web.de>
2024-05-07 14:04:32 +0200

    Commit13: cleaned code of deprecated Controller

--- src/main/java/com/openclassrooms/course/springboot/activitych3/controllers/RentsControllerOld.java
+++ src/main/java/com/openclassrooms/course/springboot/activitych3/controllers/RentsControllerOld.java
@@ -4,30 +4,26 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.openclassrooms.course.springboot.activitych3.entities.Rent;
 import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.*;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
- * This controller provides one HTTP end-point that returns the cost of renting a one bedroom apartment
- * in the city of major cities in the world in any desired currency.
- *
- * The rental cost in those cities in USD are stored in rents.csv in CSV format.
- * The exchange rate however is retrieved dynamically on the fly from an external API
- *
- * Start the application and go to http://localhost:8080/rent?city=sydney&currency=AUD on your browser
- * to see how it works
+ * @deprecated use RentsController in the same package instead.
  */
-
-
-//@RestController
+@Slf4j
+@Deprecated(since = "SB3.2.5 just for documentation purposes")
+@RestController
 public class RentsControllerOld {
 
     private List<Rent> rents;
@@ -38,13 +34,13 @@ public class RentsControllerOld {
     }
 
     /**
-     * e.g. http://localhost:8080/rent?city=sydney&currency=AUD
+     * e.g. <a href="http://localhost:8080/rent?city=sydney&currency=AUD">...</a>
      *
      * @param city the city that the apartment is located in
      * @param currency the currency we want to see the rental cost in
      * @return the calculated rental cost of a 1 BR apartment
      */
-    @GetMapping("/rent")
+    @GetMapping("/rentold")
     public Map<String, Object> getRentForCityInCurrency(@RequestParam("city") String city, @RequestParam("currency") String currency) {
 
 
@@ -56,12 +52,12 @@ public class RentsControllerOld {
         String result;
         Rent rent = findByCity(city);
         if (rent == null) {
-            System.out.println("Error: No rental data found for the city "+city);
+            log.error("Error: No rental data found for the city {}",city);
             result = "No rental data found for the city: "+city;
         } else {
             Double conversionRateToUsd = getConversionRateToUsd(currency);
             if (conversionRateToUsd == null) {
-                System.out.println("Error: No exchange rate found for the currency "+currency );
+                log.error("Error: No exchange rate found for the currency {}",currency );
                 result = "No exchange rate found for the currency "+currency;
             } else {
                 double rentInCurrency = rent.getRentPrice() * conversionRateToUsd;
@@ -85,8 +81,8 @@ public class RentsControllerOld {
     /**
      * Initializes the "rents" list by reading the values in rents.csv file
      * located in the /resources folder using Opencsv library
-     *
-     * @throws FileNotFoundException
+     * <p>
+     * Throws IOException
      */
     private void initializeCitiesList() throws IOException {
         InputStream resource = new ClassPathResource("rents.csv").getInputStream();
@@ -96,10 +92,10 @@ public class RentsControllerOld {
 
 
     /**
-     *  This calls an external API on the internet from https://currencylayer.com
-     *  It's free and you can get your own access_key from the website
+     *  This calls an external API on the internet from <a href="https://currencylayer.com">...</a>
+     *  It's free, and you can get your own access_key from the website
      *  to see and example of the API output go to:
-     *  http://apilayer.net/api/live?access_key=e8f742c7609c8d94f4d40f7d1fd104d9&currencies=AUD&format=1
+     *  <a href="http://apilayer.net/api/live?access_key=e8f742c7609c8d94f4d40f7d1fd104d9&currencies=AUD&format=1">...</a>
      *
      * @param currency that we want to see the exchange rate of
      * @return the exchange rate USD -> currency
@@ -111,13 +107,13 @@ public class RentsControllerOld {
         try {
             ResponseEntity<ObjectNode> response = template.getForEntity(apiUrl + currency.toUpperCase(), ObjectNode.class);
 
-            JsonNode rateNode = response.getBody().path("quotes").path("USD" + currency.toUpperCase());
+            JsonNode rateNode = Objects.requireNonNull(response.getBody()).path("quotes").path("USD" + currency.toUpperCase());
             if (rateNode.isMissingNode()) {
                 return null;
             }
             return rateNode.asDouble();
         } catch (Exception e) {
-            System.out.println("ERROR! Exception happened while trying to get currency!" + e.getMessage());
+            log.error("ERROR! Exception happened while trying to get currency! {}", e.getMessage());
             return null;
         }
     }
